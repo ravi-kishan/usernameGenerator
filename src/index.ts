a3763cb3fd88d1e7d2151f4f82488a147da624e4
ravi <ravikishanaka@gmail.com>
2020-07-24 13:01:56 +0530

    final changes

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,57 @@
 import { camelCase } from "lodash-es";
 import { EventEmitter } from "events";
-import * as $ from "jquery";
+import $ from "jquery";
+import { cached } from "@ravik1/locator";
 import { randomNum } from "./math";
 
 const emittor = new EventEmitter();
 var username = "";
 
-emittor.on('generated',function(){ 
-   // alert("The generated username is " + username);
+var para = document.createElement("P");               
+para.innerText = "Enter name to generate username";               
+document.body.appendChild(para);  
+
+var inpt = document.createElement("INPUT");
+inpt.setAttribute("type", "text");
+inpt.setAttribute("id", "name");
+document.body.appendChild(inpt);
+
+emittor.on('generated', function () {
+     console.log("The generated username is " + username);
 });
+
+var btn = document.createElement("BUTTON");   
+btn.innerHTML = "GENERATE";
+document.body.appendChild(btn); 
+
+var show = document.createElement("P"); 
+document.body.appendChild(show);  
+
 
 var hint = "Another alternative is ";
 
 export function generate(myArg:string){
-    var name = null;// (<HTMLInputElement>document.getElementById("fname")).value;
+    var name = null;
     if(name == null ) {
         name = myArg;
     }
     username = camelCase(name + randomNum(10));
     var temp = camelCase(randomNum(10) + name);
     console.log(username);
-    emittor.emit('generated');
     return username;
-    // $(document).ready(function(){
-    //     $('#hint').text(hint + temp);
-    //   });
 }
+
+btn.onclick = function() {
+    var name = (<HTMLInputElement>document.getElementById("name")).value;
+    username = camelCase(name + randomNum(10));
+    var temp = camelCase(randomNum(10) + name);
+    show.innerText = "The generated username is " + username;
+     $(document).ready(function(){
+         $('#hint').text(hint + temp);
+       });
+}
+
+cached("@ravik1/usernamegenerator");
 
 
 
--- src/main.test.ts
+++ src/main.test.ts
@@ -1,16 +1,36 @@
 import { expect } from "chai";
-import { generate } from "./index";
-import { strict } from "assert";
+import * as fs from 'fs';
+import * as path from 'path';
+import { JSDOM } from "jsdom";
+
+const dom:any = new JSDOM("<!DOCTYPE html><html><head></head><body></body></html>");
+global.window = dom.window;
+global.document = dom.window.document;
+
+
+
+
 
 
 describe('usernemeGenerator test', () => {
+
+    var result1:any,result2:any;
+    before(() => {
+        return new Promise((resolve) => {
+            import('./index')
+            .then(content => {
+                result1 = content.generate("Ra vi");
+                result2 = content.generate("Ravi");
+              
+            })
+            resolve();
+        });
+      });
  it('should return some username', () => {
-        var result = generate("Ra vi");
         var value = true;
         expect(value).to.equal(true);
     });
  it('should return some username', () => {
-        var result = generate("Ravi");
         var value = true;
         expect(value).to.equal(true);
     });
